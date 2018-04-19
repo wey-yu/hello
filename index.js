ac0f372024eb614ed2d02ba1cc9106750190af01
Philippe Charrière <ph.charriere@gmail.com>
2018-04-19 13:32:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ service.get({uri:`/`, f: (request, response) => {
         <section class="container">
           <div>
             <h1 class="title">
-              👋 Hello 🌍 Devoxx
+              👋 Hello 🌍 World
             </h1>
             <h2 class="subtitle">
               made with ❤️
